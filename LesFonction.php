15e26fc55066fc908968a60a3b02841dfab3217e
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-07-29 17:34:21 +0200

    Les fonctions en PHP

---
+++ LesFonction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Les fonctioncs en php</title>
+    </head>
+
+    <body>
+        <?php
+
+      
+
+       
+
+        function Multiplication($x,$y){
+            return $x*$y;
+         }
+
+         $resultat = Multiplication(7,5);// je stocke le résultat de la fonction dans une variable
+         echo $resultat;//ouis j'affiche le résultat 
+
+
+        ?>
+    </body>
+</html>
