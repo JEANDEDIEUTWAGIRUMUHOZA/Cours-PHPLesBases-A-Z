ef2ba3095260a768296082f644a15efaf3761ae2
Jean de Dieu TWAGIRUMUHOZA <jeandedieu.emploi@gmail.com>
2020-07-29 13:20:34 +0200

    initial commit

---
+++ LesBases.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Les Bases en PHP</title>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <?php
+    
+echo"ceci a été écrit en PHP";
+    ?>
+     <br>
+    <?php
+    echo"ceci est toujour en PHP";
+    ?>
+</body>
+</html>
